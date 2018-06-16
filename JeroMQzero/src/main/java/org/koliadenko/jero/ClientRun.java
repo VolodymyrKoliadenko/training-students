cfd319b7a70f68272853a9e209da07ea9a994e4a
VolodymyrKoliadenko <vovakkk@yandex.ua>
2018-06-16 21:23:33 +0300

    2
    Тест, хоть и бред

--- JeroMQzero/src/main/java/org/koliadenko/jero/ClientRun.java
+++ JeroMQzero/src/main/java/org/koliadenko/jero/ClientRun.java
@@ -8,23 +8,27 @@ public class ClientRun implements Runnable {
 
     @Override
     public void run() {
+       
         MDClientapi clientSession = new MDClientapi("tcp://localhost:5555", true);
 
         int count;
         for (count = 0; count < 4; count++) {
+            
             try {
+                Thread.sleep(90);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ClientRun.class.getName()).log(Level.SEVERE, null, ex);
+            }
                 ZMsg request = new ZMsg();
                 request.addString("Hello world in runnable");
-                Thread.sleep(300);
+                
                 ZMsg reply = clientSession.send("echo", request);
                 if (reply != null) {
                     reply.destroy();
                 } else {
                     break; // Interrupt or failure
                 }
-            } catch (InterruptedException ex) {
-                Logger.getLogger(ClientRun.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            
         }
 
         System.out.printf("%d requests/replies processed\n", count);
--- JeroMQzero/src/main/java/org/koliadenko/jero/Main.java
+++ JeroMQzero/src/main/java/org/koliadenko/jero/Main.java
@@ -1,9 +1,5 @@
 package org.koliadenko.jero;
 
-import org.zeromq.ZContext;
-import org.zeromq.ZMQ;
-import org.zeromq.ZMQ.Socket;
-
 /**
  * Ноябрь 2017
  *
@@ -16,27 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        try (ZContext context = new ZContext()) {
-            Socket socket = context.createSocket(ZMQ.REP);
-            socket.bind("tcp://*:5555");
-            System.out.println(socket.getLastEndpoint());
-            
-            (new ClientRun()).run();
-
-            while (!Thread.currentThread().isInterrupted()) {
-                System.out.println("__ ruNNNN");
-
-                byte[] reply = socket.recv(0);
-
-                // Print received message
-                System.out.println(
-                        "MAIN loop=> " + ": " + new String(reply, ZMQ.CHARSET));
-
-                // Send a response
-                String response = "Hello, Vova";
-                socket.send(response.getBytes(ZMQ.CHARSET), 0);
-            }
-        }
+        (new ClientRun()).run();
+        (new SocketRun()).run();
     }
-
 }
